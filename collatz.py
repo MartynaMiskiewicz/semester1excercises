3bdd2fd1808bd118e6237ef86d7ebba7776f9622
MartynaMisk <36375583+MartynaMisk@users.noreply.github.com>
2018-02-15 20:55:28 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -3,10 +3,10 @@
 
 n = 125
 print(n)
-while n != 1:
+while n != 1:  #if number is odd 
   if n % 2 == 1:
-    n = n * 3 + 1
+    n = n * 3 + 1 
     print(n)
-  elif n % 2 == 0:
+  elif n % 2 == 0:  #if number is even
     n = n // 2
     print(n) 
