c0b12963f5fe122e3188d67796d502dbebea5ade
MartynaMiskiewicz <36374839+MartynaMiskiewicz@users.noreply.github.com>
2018-04-07 20:35:32 +0100

    Update exercise5openfile.py

--- exercise5openfile.py
+++ exercise5openfile.py
@@ -1,4 +1,7 @@
-with open("data/iris.csv") as f:
+# Martyna Miskiewicz
+# Exercise 5 on how to open a file in a desired format. File was copied from Fishers iris data http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data 
+
+with open("data/iris.csv") as f:  # this line opens the file from forlder called data
   for line in f:
-   column = (line.split(',')[:4])
+   column = (line.split(',')[:4]) # this opens the first 4 columns 
    print(column)
