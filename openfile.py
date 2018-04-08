fa7519d7185ff3d1c7e72e6e9f71a5acc78cc36f
MartynaMiskiewicz <36374839+MartynaMiskiewicz@users.noreply.github.com>
2018-04-08 11:14:35 +0100

    Create openfile.py

---
+++ openfile.py
@@ -0,0 +1,7 @@
+# Martyna Miskiewicz
+# Exercise 5 on how to open a file in a desired format. File was copied from Fishers iris data http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data 
+
+with open("data/iris.csv") as f:  # this line opens the file from forlder called data
+  for line in f:
+   column = (line.split(',')[:4]) # this opens the first 4 columns, however it does not print it perfectly. They contain unwated characters which I am not sure how to eliminate 
+   print(column)
